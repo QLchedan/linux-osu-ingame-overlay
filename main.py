4921493ba03a0684403811c21414015ff6cdab10
QLchedan <45850458+QLchedan@users.noreply.github.com>
2026-05-02 19:59:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import overlay_window
+from dbus.mainloop.glib import DBusGMainLoop
 
 if __name__ == "__main__":
+    DBusGMainLoop(set_as_default=True)
+    
     app = overlay_window.GameOverlay()
     app.run()
